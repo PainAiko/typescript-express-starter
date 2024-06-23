93b7292bd560fc37b38f2c864df6dde9015516d9
dadou.ai <Zidane@foot.fr>
2024-06-23 13:40:42 +0300

    ignore node module

--- dist/index.js
+++ dist/index.js
@@ -7,7 +7,7 @@ const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
 app.get('/', (req, res) => {
-    res.send('Hello, TypeScript with Express!');
+    res.send('Hello, TypeScript with Express !');
 });
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
