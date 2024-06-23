623c5202dbbba9dacbc67a9ecb4541a8e633981a
dadou.ai <Zidane@foot.fr>
2024-06-23 13:56:52 +0300

    add middleware for handle resquet body and form

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
 app.get('/', (req, res) => {
   res.send('Hello, TypeScript with Express !');
 });
