b631cca05fa5638de2af9d2821d3919dfe28dd46
dadou.ai <Zidane@foot.fr>
2024-06-23 18:54:53 +0300

    create controllers

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,5 +1,6 @@
 import { RequestHandler } from "express";
 
 export const createNewUser:RequestHandler = (req,res) =>{
+    console.log(req.body);
     res.send('OK')
 }
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { createNewUser } from "src/controllers/auth.controller";
+import { createNewUser } from "@controllers/auth.controller";
 const AuthRouter = Router()
 
 AuthRouter.post('/sign-up',createNewUser)
-AuthRouter.post('/sign-ip',)
-AuthRouter.post('/verify',)
-AuthRouter.post('/refresh-token',)
+AuthRouter.post('/sign-ip')
+AuthRouter.post('/verify')
+AuthRouter.post('/refresh-token')
 export default AuthRouter
